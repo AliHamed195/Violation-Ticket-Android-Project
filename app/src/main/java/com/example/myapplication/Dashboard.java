94073e6bcb9868ab93f086c9bcaf823e4ff82ca4
Ali Hamed <alihamed.cg@gmail.com>
2022-12-25 15:01:57 +0200

    creating the dashboard eimple layout with its actions and create two empty activitys for create violation and see all violations

--- app/src/main/java/com/example/myapplication/Dashboard.java
+++ app/src/main/java/com/example/myapplication/Dashboard.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    Button seeAllViolations, createViolation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // find the buttons in the layout
+        seeAllViolations = findViewById(R.id.seeAllViolationsSectionBtn);
+        createViolation = findViewById(R.id.createViolationSectionBtn);
+
+        // Adding the actions to the buttons.
+        seeAllViolations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this,CreateViolationStartPage.class));
+            }
+        });
+
+        createViolation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Dashboard.this,AllViolationsPage.class));
+            }
+        });
+
     }
 }
