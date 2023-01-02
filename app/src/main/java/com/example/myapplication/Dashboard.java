05546af139052b6c8f5fefeec7f4b89482f4c804
Ali Hamed <alihamed.cg@gmail.com>
2023-01-02 22:59:46 +0200

    finish the menu style

--- app/src/main/java/com/example/myapplication/Dashboard.java
+++ app/src/main/java/com/example/myapplication/Dashboard.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     Button seeAllViolations, createViolation;
 
@@ -17,6 +23,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         // find the buttons in the layout
+        drawerLayout = findViewById(R.id.drawer_layout);
         seeAllViolations = findViewById(R.id.seeAllViolationsSectionBtn);
         createViolation = findViewById(R.id.createViolationSectionBtn);
 
@@ -35,5 +42,21 @@ public class Dashboard extends AppCompatActivity {
             }
         });
 
+        //to make the button for the menu
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
