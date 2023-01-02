296faf2bbc4abe9672982f14d13431c4620d4036
Ali Hamed <alihamed.cg@gmail.com>
2023-01-03 00:15:06 +0200

    create the page five and six for creating violation

--- app/src/main/java/com/example/myapplication/CreateViolationPageFive.java
+++ app/src/main/java/com/example/myapplication/CreateViolationPageFive.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class CreateViolationPageFour extends AppCompatActivity {
+public class CreateViolationPageFive extends AppCompatActivity {
 
     Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_violation_page_four);
+        setContentView(R.layout.activity_create_violation_page_five);
 
-        nextPage = findViewById(R.id.nextPageFourCreateViolation);
+        nextPage = findViewById(R.id.nextPageFiveCreateViolation);
 
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),CreateViolationPageFive.class));
+                startActivity(new Intent(getApplicationContext(),CreateViolationPageSix.class));
             }
         });
     }
