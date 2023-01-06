5d9f42acf28724385d858ab9caeef0461eabe5cd
Ali Hamed <alihamed.cg@gmail.com>
2023-01-06 21:39:03 +0200

    finish the spinner automation

--- app/src/main/java/com/example/myapplication/CreateViolationPageThree.java
+++ app/src/main/java/com/example/myapplication/CreateViolationPageThree.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -24,11 +25,43 @@ public class CreateViolationPageThree extends AppCompatActivity {
         violationTypes = findViewById(R.id.violationTypes);
         subject = findViewById(R.id.violationSubject);
 
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.type_Violation, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterListOne=ArrayAdapter.createFromResource(this, R.array.type_Violation, android.R.layout.simple_spinner_item);
+        adapterListOne.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
-        violationTypes.setAdapter(adapter);
-        subject.setAdapter(adapter);
+        violationTypes.setAdapter(adapterListOne);
+
+        violationTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            ArrayAdapter<CharSequence> newAdapter;
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(violationTypes.getSelectedItem().toString().equals("فئة أولى")){
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.type_Violation1, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }else if(violationTypes.getSelectedItem().toString().equals("فئة ثانية")){
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.type_Violation2, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }else if(violationTypes.getSelectedItem().toString().equals("فئة ثالثة")){
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.type_Violation3, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }else if(violationTypes.getSelectedItem().toString().equals("فئة رابعة")){
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.type_Violation4, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }else if(violationTypes.getSelectedItem().toString().equals("فئة خامسة")){
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.type_Violation5, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }else{
+                    newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.choseBefore, android.R.layout.simple_spinner_item);
+                    subject.setAdapter(newAdapter);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                newAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.choseBefore, android.R.layout.simple_spinner_item);
+                subject.setAdapter(newAdapter);
+            }
+        });
+
 
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
